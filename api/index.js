63a29458be880315f8ec9b852b5ca24245e85fcb
Lincoln Nguyen <letsmakeaworld@icloud.com>
2022-04-13 04:11:22 -0400

    update

--- api/index.js
+++ api/index.js
@@ -290,7 +290,7 @@ const truncateString = (str, num) => {
   if (str.length <= num) {
     return str;
   }
-  return str.slice(0, num) + '...';
+  return str.slice(0, num);
 };
 
 router.get('/get-novel-text', (req, res) => {
